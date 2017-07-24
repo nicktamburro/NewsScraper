762a6bf286265739325359fa5bfb9d86f206260b
Nick Tamburro <Nick Tamburro>
2017-07-23 23:37:04 -0700

    scrapes working

---
+++ BenExample.js
@@ -0,0 +1,196 @@
+/* Showing Mongoose's "Populated" Method (18.3.8)
+ * INSTRUCTOR ONLY
+ * =============================================== */
+
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/week18day3mongoose");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+// Routes
+// ======
+
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.echojs.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+// Grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  var newNote = new Note(req.body);
+
+  // And save the new note the db
+  newNote.save(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's note
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
+Add Comment Collapse
+
+
+
+Jump
+6 new messages since 12:52 PM
+Mark as read (esc)
+
+Message #_general
+
+*bold* _italics_ ~strike~ `code` ```preformatted``` >quote
+Files
+
+nicktamburro
+server.js  
+Edit  More actions
+// Students: Using this template, the cheerio documentation,
+// and what you've learned in class so far, scrape a website
+// of your choice, save information from the page in a result array, and log it to the console.
+
+var cheerio = require("cheerio");
+var request = require("request");
+
+// Make a request call to grab the HTML body from the site of your choice
+request("http://pitchfork.com/reviews/albums/", function(error, response, html) {
+
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(html);
+
+  // An empty array to save the data that we'll scrape
+  var results = [];
+
+  // Select each element in the HTML body from which you want information.
+  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+  // but be sure to visit the package's npm page to see how it works
+  $("div.review").each(function(i, element) {
+
+    var href = $(element).children().text("href");
---
+++ server.js
@@ -0,0 +1,149 @@
+var cheerio = require("cheerio");
+  var request = require("request");
+//var mongojs = require("mongojs");
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+mongoose.Promise = Promise;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//what is this?
+mongoose.connect("mongodb://localhost/");
+var db = mongoose.connection;
+
+db.once("open", function(){
+  console.log("Mongoose connection successful.");
+})
+
+//var databaseUrl = "scraper";
+//var collections = ["scrapedData"];
+
+app.get("/scrape", function(req, res){
+// First, tell the console what server.js is doing
+console.log("\n***********************************\n" +
+            "Getting cover art, artist, and album title from Pitchfork.com's latest reviews:" +
+            "\n***********************************\n");
+// Making a request for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
+request("http://pitchfork.com/reviews/albums/", function(error, response, html) {
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+  var $ = cheerio.load(html);
+  // An empty array to save the data that we'll scrape
+ 
+  // With cheerio, find each p-tag with the "title" class
+  // (i: iterator. element: the current element)
+  $("div.review").each(function(i, element) {
+
+     var result = {};
+    // Save the text of the element in a "title" variable
+    result.coverArt = $(element).children().children().children().children().attr("src");
+    result.artist = $(element).children().children().children().find("li").text();
+    result.title = $(element).children().children().find("h2.title").text();
+    // In the currently selected element, look at its child elements (i.e., its a-tags),
+    // then save the values for any "href" attributes that the child elements may have
+    //var link = $(element).children().attr("href");
+    // Save these results in an object that we'll push into the results array we defined earlier
+    console.log(result.coverArt);
+    console.log(result.artist);
+    console.log(result.title);
+
+    var coverArt = result.coverArt;
+    var artist = result.artist;
+    var title = result.title;
+
+    var entry = new Article(result);
+
+    entry.save(function(err, doc){
+      if (err){
+        console.log(err);
+      }
+      else {
+        console.log(doc);
+      }
+    });
+
+  });
+});
+res.send("Scrape Complete");
+
+});
+    
+   /* results.push({
+      coverArt: coverArt,
+      artist: artist,
+      title: title
+      //link: link
+    });
+  });
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
+});*/
+
+app.get("/articles", function(req, res){
+  Article.find({})
+  .populate("article")
+  .exec(function(error, doc){
+    if (error){
+      console.log(error);
+    } else {
+      res.json(doc);
+    }
+  })   
+});
+
+
+app.get("/articles/:id", function(req, res) {
+  Article.findOne({"_id": req.params.id})
+
+  .populate("note")
+
+  .exec(function(error, doc){
+    if (error){
+      console.log(error);
+    }
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+app.post("/articles/:id", function(req, res){
+  var newNote = new Note(req.body);
+
+  newNote.save(function(error, doc){
+    if (error){
+      console.log(error);
+    }
+    else{
+      Article.findOneAndUpdate({"_id": req.params.id }, {"note": doc._id})
+      .exec(function(err, doc){
+        if (err){
+          console.log(err);
+        }
+        else {
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(3000, function(){
+  console.log("App running on port 3000!");
+})
